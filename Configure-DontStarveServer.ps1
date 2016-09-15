3c138403e6e33c9cf4d15afe15ccc7b6f0c42856
Michael Kropat <mail@michael.kropat.name>
2016-09-15 00:37:36 +0000

    Populate leveldataoverride.lua

--- Configure-DontStarveServer.ps1
+++ Configure-DontStarveServer.ps1
@@ -110,6 +110,122 @@ authentication_port = 8768
 
 }
 
+if (-not (Test-Path $clusterDir\Master\leveldataoverride.lua)) {
+
+@"
+return {
+  desc="The standard Don't Starve experience.",
+  hideminimap=false,
+  id="SURVIVAL_TOGETHER",
+  location="forest",
+  max_playlist_position=999,
+  min_playlist_position=0,
+  name="Default",
+  numrandom_set_pieces=5,
+  override_level_string=false,
+  overrides={
+    alternatehunt="default",
+    angrybees="default",
+    autumn="default",
+    bearger="default",
+    beefalo="default",
+    beefaloheat="default",
+    bees="default",
+    berrybush="default",
+    birds="default",
+    boons="default",
+    branching="default",
+    butterfly="default",
+    buzzard="default",
+    cactus="default",
+    carrot="default",
+    catcoon="default",
+    chess="default",
+    day="default",
+    deciduousmonster="default",
+    deerclops="default",
+    dragonfly="default",
+    flint="default",
+    flowers="default",
+    frograin="default",
+    goosemoose="default",
+    grass="default",
+    houndmound="default",
+    hounds="default",
+    hunt="default",
+    krampus="default",
+    layout_mode="LinkNodesByKeys",
+    liefs="default",
+    lightning="default",
+    lightninggoat="default",
+    loop="default",
+    lureplants="default",
+    marshbush="default",
+    merm="default",
+    meteorshowers="default",
+    meteorspawner="default",
+    moles="default",
+    mushroom="default",
+    penguins="default",
+    perd="default",
+    pigs="default",
+    ponds="default",
+    prefabswaps="default",
+    prefabswaps_start="default",
+    rabbits="default",
+    reeds="default",
+    regrowth="default",
+    roads="default",
+    rock="default",
+    rock_ice="default",
+    sapling="default",
+    season_start="default",
+    spiders="default",
+    spring="default",
+    start_location="default",
+    summer="default",
+    tallbirds="default",
+    task_set="default",
+    tentacles="default",
+    touchstone="default",
+    trees="default",
+    tumbleweed="default",
+    walrus="default",
+    weather="default",
+    wildfires="default",
+    winter="default",
+    world_size="default",
+    wormhole_prefab="wormhole" 
+  },
+  random_set_pieces={
+    "Chessy_1",
+    "Chessy_2",
+    "Chessy_3",
+    "Chessy_4",
+    "Chessy_5",
+    "Chessy_6",
+    "ChessSpot1",
+    "ChessSpot2",
+    "ChessSpot3",
+    "Maxwell1",
+    "Maxwell2",
+    "Maxwell3",
+    "Maxwell4",
+    "Maxwell5",
+    "Maxwell6",
+    "Maxwell7",
+    "Warzone_1",
+    "Warzone_2",
+    "Warzone_3" 
+  },
+  required_prefabs={ "multiplayer_portal" },
+  substitutes={  },
+  version=2 
+}
+"@ | Out-File -Encoding ascii $clusterDir\Master\leveldataoverride.lua
+
+}
+
 New-Item -ItemType Directory -Force -Path $clusterDir\Caves | Out-Null
 
 if (-not (Test-Path $clusterDir\Caves\server.ini)) {
@@ -126,6 +242,75 @@ name = Caves
 master_server_port = 27019
 authentication_port = 8769
 "@ | Out-File -Encoding ascii $clusterDir\Caves\server.ini
+
+}
+
+if (-not (Test-Path $clusterDir\Caves\leveldataoverride.lua)) {
+
+@"
+return {
+  background_node_range={ 0, 1 },
+  desc="Delve into the caves... together!",
+  hideminimap=false,
+  id="DST_CAVE",
+  location="cave",
+  max_playlist_position=999,
+  min_playlist_position=0,
+  name="The Caves",
+  numrandom_set_pieces=0,
+  override_level_string=false,
+  overrides={
+    banana="default",
+    bats="default",
+    berrybush="default",
+    boons="default",
+    branching="default",
+    bunnymen="default",
+    cave_ponds="default",
+    cave_spiders="default",
+    cavelight="default",
+    chess="default",
+    earthquakes="default",
+    fern="default",
+    fissure="default",
+    flint="default",
+    flower_cave="default",
+    grass="default",
+    layout_mode="RestrictNodesByKey",
+    lichen="default",
+    liefs="default",
+    loop="default",
+    marshbush="default",
+    monkey="default",
+    mushroom="default",
+    mushtree="default",
+    prefabswaps="default",
+    prefabswaps_start="default",
+    reeds="default",
+    regrowth="default",
+    roads="never",
+    rock="default",
+    rocky="default",
+    sapling="default",
+    season_start="default",
+    slurper="default",
+    slurtles="default",
+    start_location="caves",
+    task_set="cave_default",
+    tentacles="default",
+    touchstone="default",
+    trees="default",
+    weather="default",
+    world_size="default",
+    wormhole_prefab="tentacle_pillar",
+    wormlights="default",
+    worms="default" 
+  },
+  required_prefabs={ "multiplayer_portal" },
+  substitutes={  },
+  version=2 
+}
+"@ | Out-File -Encoding ascii $clusterDir\Caves\leveldataoverride.lua
 
 }
 
